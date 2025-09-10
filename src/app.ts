9623fe9ad55c49883c43f8abdc174c7b3f2a6df2
Etienne Fuh <36563272+THISmann@users.noreply.github.com>
2025-09-10 17:14:17 +0300

    test

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 // src/app.ts
 import knex, { migrate, seed } from "./postgres/knex.js";
-import { SpreadsheetService } from "./services/pgService.js";
-import { GoogleSheetsService, SheetData } from "./services/googlesheetService.js";
-import { WbTariffService } from "./services/WbTariffService.js";
+import { SpreadsheetService } from "./services/pgService";
+import { GoogleSheetsService, SheetData } from "./services/googlesheetService";
+import { WbTariffService } from "./services/WbTariffService";
 import cron from "node-cron";
 
 const tariffService = new WbTariffService();
